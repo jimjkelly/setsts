3c54f4fc9fbac67c937b8014b59e11e8831f8fa8
Jim Kelly <pthread1981@gmail.com>
2019-05-27 12:10:34 +0200

    Initial Tool Creation
    Does the basics, saves a little time by reformatting token
    generation output.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # setsts
 A simple tool for setting AWS STS tokens
+
+## What does this do?
+
+Currently not much - in truth this project is more for me to mess with Go a bit.
+Basically this reformats output of STS token generation so it can be copied
+directly into your `~/.aws/credentials` file.
+
+## How to install
+
+Man who knows.  I guess clone the repo and then `go get ./...` to install
+dependencies and then `go build -o setsts main.go`.  Still not up to speed
+on how this Go stuff works.
+
+## Usage
+
+Usage flags can be seen by running `setsts -h` but here's the gist:
+
+```
+$ setsts --serial-number <YOUR MFA SERIAL> 123456
+```
+
+where `123456` is your MFA token.
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+)
+
+func main() {
+	profile := flag.String("-profile", "default", "Name of the AWS profile to use.")
+	serial := flag.String("-serial-number", "", "The serial number of your MFA device.")
+	duration := flag.Int64("-duration-seconds", 43200, "The amount of time your generated STS token is valid for.")
+	flag.Parse()
+
+	code := flag.Arg(0)
+
+	os.Setenv("AWS_PROFILE", *profile)
+
+	sess := session.Must(session.NewSession())
+	_, err := sess.Config.Credentials.Get()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error retrieving credentials.")
+		os.Exit(1)
+	}
+
+	svc := sts.New(session.New())
+	input := &sts.GetSessionTokenInput{
+		DurationSeconds: aws.Int64(*duration),
+		SerialNumber:    aws.String(*serial),
+		TokenCode:       aws.String(code),
+	}
+
+	result, err := svc.GetSessionToken(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case sts.ErrCodeRegionDisabledException:
+				fmt.Println(sts.ErrCodeRegionDisabledException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println("aws_access_key_id = ", *result.Credentials.AccessKeyId)
+	fmt.Println("aws_secret_access_key = ", *result.Credentials.SecretAccessKey)
+	fmt.Println("aws_session_token = ", *result.Credentials.SessionToken)
+}
